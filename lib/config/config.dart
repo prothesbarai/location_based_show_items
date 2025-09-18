99b80f4639cedad94f0bd32c3d3cb7124e9573bc
Prothes Barai <aspprothesbarai1619@gmail.com>
2025-09-18 17:37:37 +0600

    asp prothes

--- lib/config/config.dart
+++ lib/config/config.dart
@@ -4,7 +4,7 @@ class Config {
   static double get horizontalPaddingShop => 5;
   static double get gapShop => 2;
 
-/// >>> For Product Grid Margin Control
+  /// >>> For Product Grid Margin Control
   static int get verticalItemGridProduct => 3;
   static double get horizontalPaddingProduct => 5;
   static double get gapProduct => 2;
